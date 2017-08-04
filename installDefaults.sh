451a36f5bcf55261387736e97413e211e8123331
Sandeep Kotha <akotha31@gmail.com>
2017-08-04 14:06:32 +0100

    update scripts to install maven

--- installDefaults.sh
+++ installDefaults.sh
@@ -3,27 +3,17 @@
 sudo apt-get -y update
 sudo apt-get purge openjdk* default-jre
 sudo apt-get remove docker docker-engine docker.io
-#sudo apt-get -y update
-sudo apt-get -y install postgresql-9.4
-sudo apt-get -y install postgresql-client
+#sudo apt-get -y install postgresql-9.4
+#sudo apt-get -y install postgresql-client
 sudo apt-get -y install git
 sudo apt-get -y install curl
 sudo apt-get -y install git-flow
+sudo apt install -y unzip
 sudo apt-get -y install linux-image-extra-$(uname -r)
 sudo apt-get -y install linux-image-extra-virtual
 sudo apt-get -y install apt-transport-https ca-certificates software-properties-common
-
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-#sudo apt-get -y update
-
-#sudo apt-get -y install docker-ce
 sudo apt install -y python-pip
-#sudo pip install --upgrade pip
-#sudo pip uninstall docker-py
-#sudo pip uninstall docker
-#sudo pip install docker
+sudo pip install --upgrade pip
 
 
 curl https://releases.rancher.com/install-docker/17.03.sh | sh
@@ -41,25 +31,26 @@ sudo update-alternatives --install /usr/bin/javac javac /home/java/jdk1.8.0_131/
 sudo update-alternatives --config java
 sudo update-alternatives --config javac
 
-sudo cat > /etc/profile.d/oraclejdk.sh <<-EOF
-export J2SDKDIR=/home/java/jdk1.8.0_131
-export J2REDIR=/home/java/jdk1.8.0_131/jre
-export PATH=$PATH:/home/java/jdk1.8.0_131/bin:/home/java/jdk1.8.0_131/db/bin:/home/java/jdk1.8.0_131/jre/bin
-export JAVA_HOME=/home/java/jdk1.8.0_131
-export DERBY_HOME=/home/java/jdk1.8.0_131/db
-EOF
-
-echo "path JDK path set complete"
-
-sudo sh /etc/profile.d/oraclejdk.sh
-
-echo "source to JDK path complete endof script"
-
 sudo wget https://services.gradle.org/distributions/gradle-4.0.2-bin.zip
 sudo mkdir /opt/gradle
 sudo unzip -d /opt/gradle/ gradle-4.0.2-bin.zip
 
-sudo export PATH=$PATH:/opt/gradle/gradle-4.0.2/bin
+sudo wget http://mirror.vorboss.net/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.zip
+sudo mkdir /opt/maven
+sudo unzip -d /opt/maven/ apache-maven-3.5.0-bin.zip
+
+sudo cat > /etc/profile.d/oraclejdk.sh <<-EOF
+export J2SDKDIR=/home/java/jdk1.8.0_131
+export J2REDIR=/home/java/jdk1.8.0_131/jre
+export JAVA_HOME=/home/java/jdk1.8.0_131
+export DERBY_HOME=/home/java/jdk1.8.0_131/db
+export PATH=$PATH:/home/java/jdk1.8.0_131/bin:/home/java/jdk1.8.0_131/db/bin:/home/java/jdk1.8.0_131/jre/bin:/opt/gradle/gradle-4.0.2/bin:/opt/maven/apache-maven-3.5.0/bin
+EOF
+
+
+sudo sh /etc/profile.d/oraclejdk.sh
+
+echo "source to JDK path complete endof script"
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
